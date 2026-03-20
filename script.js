b624d4ca59c7b3cee50f95a94f21ee6c5326fa87
NOTFARIHZ <farihz13th@gmail.com>
2026-03-21 00:40:50 +0800

    harvard hahaha

--- script.js
+++ script.js
@@ -1,20 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
 
-  // This is a "Classic" script - no modules, no imports
-  console.log("Classic JS is running!");
-  
-  let count = 0;
-  // We'll create a new element just to test
-  const testDiv = document.createElement('div');
-  testDiv.style.position = 'fixed';
-  testDiv.style.top = '0';
-  testDiv.style.background = 'red';
-  testDiv.innerText = "JS IS ALIVE";
-  document.body.appendChild(testDiv);
 
-  setInterval(() => {
-    count++;
-    testDiv.innerText = "Seconds: " + count;
-  }, 1000);
 
 });
